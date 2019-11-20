49aa151ba5c4b2504399a48decd84113996fc9be
gaoac <gaoac.snow@outlook.com>
2019-11-20 20:16:31 +0800

    🔧 Config(gatsby-config.js): 更新gatsby-plugin-eslint配置

--- gatsby-config.js
+++ gatsby-config.js
@@ -88,7 +88,7 @@ module.exports = {
       options: {
         test: /\.js$|\.jsx$/,
         exclude: /(node_modules|.cache|public)/,
-        // stages: ['develop'],
+        stages: ['develop'],
         options: {
           emitWarning: true,
           failOnError: false,
