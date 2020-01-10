8e2591000bce3f8caee02d3b495eb954f90f0aca
gaoac <gaoac.snow@outlook.com>
2020-01-10 11:22:51 +0800

    ✨ Feat: 新增 google-analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,23 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: 'UA-156021481-1',
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: [],
+        // Any additional optional fields
+        cookieDomain: 'gaoac.netlify.com',
+      },
+    },
+    {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/content/blog`,
